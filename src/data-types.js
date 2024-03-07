49eca07ece742fceb054ee72614bc4702c542378
Stefano Kusuma Ali <stefano8795@gmail.com>
2024-03-07 19:05:35 +0800

    finish lesson

--- src/data-types.js
+++ src/data-types.js
@@ -7,14 +7,31 @@ console.log("str",typeof str);
 const bool = true;
 console.log("bool",typeof bool);
 
-let x;
+let x; //undefined because no value is assigned
 console.log("x",typeof x);
 
-const y = null;
+const y = null; //object, null is considered an object, can be placeholder for value
 console.log("y",typeof y);
 
-const obj = {};
+const obj = {}; //object, key-value pairs
 console.log("obj",typeof obj);
 
-const arr = [];
+const arr = []; //object, array is object that contains set of elements
 console.log("arr",typeof arr);
+
+//loop
+let defaults = [1, 2, 3]
+for (let i = 0; i < defaults.length; i++){
+    console.log(defaults[i])
+}
+
+//loop keys  of an object
+//use for-in loop
+let person = {
+    name: "joni", 
+    age: 3
+}
+
+for (let key in person){
+    console.log(key)
+}
